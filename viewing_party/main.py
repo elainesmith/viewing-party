c324b9985fe9714d23bc80f0a92fa00ed0d7c6e7
Elaine Smith <oneile1219@gmail.com>
2022-03-17 14:31:10 -0700

    passed wave 1 test1

--- viewing_party/main.py
+++ viewing_party/main.py
@@ -1,1 +1,13 @@
-# This is a test
+
+def create_movie(movie_title, genre, rating):
+    new_movie = {}
+    keys = ["title", "genre", "rating"]
+    values = [movie_title, genre, rating]
+    valid_input = False
+
+    if len(keys) == len(values):
+        valid_input = True
+        new_movie = dict(zip(keys, values))
+        return new_movie
+    while not valid_input:
+        return None
