375bd62435b9f941ae8c9dd09662863c5c0c8ce5
Elaine Smith <oneile1219@gmail.com>
2022-03-21 16:57:08 -0700

    created watch_movie function, updated syntax on test files, completed Wave 01

--- viewing_party/main.py
+++ viewing_party/main.py
@@ -11,10 +11,20 @@ def create_movie(movie_title, genre, rating):
 
 
 def add_to_watched(user_data, movie):
-    user_data["watched"] == user_data["watched"].append(movie)
+    user_data["watched"].append(movie)
     return user_data
 
 
 def add_to_watchlist(user_data, movie):
-    user_data["watchlist"] == user_data["watchlist"].append(movie)
+    user_data["watchlist"].append(movie)
     return user_data
+
+
+def watch_movie(user_data, title):
+    for movie in user_data["watchlist"]:
+        for category, item in movie.items():
+            if category == "title" and item == title:
+                user_data["watchlist"].remove(movie)
+                user_data["watched"].append(movie)
+    return user_data
+
